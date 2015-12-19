c44af2c4cda43db41f23157870fb66bff7e3712d
keezia <keeziallen@gmail.com>
2015-12-19 00:14:30 +0000

    use this to add an administrator in your terminal as pages are secure and can only be accesed by a user

---
+++ admin.sql
@@ -0,0 +1,2 @@
+/*create an administrator in the terminal*/
+INSERT INTO User(first_name, last_name, username, position,  password) VALUES('Keezia',' Allen', 'Admin', 'Administrator', 'adminKallen11');
